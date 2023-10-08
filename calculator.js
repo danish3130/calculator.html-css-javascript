fe3acf678c9b3b4559210d7092e9ad6a68937fc2
Mohd Danish <123272168+danish3130@users.noreply.github.com>
2023-10-08 18:16:23 +0530

    Add files via upload

---
+++ calculator.js
@@ -0,0 +1,7 @@
+function sound1(){
+
+    var snd = new Audio('https://commondatastorage.googleapis.com/codeskulptor-assets/jump.ogg')//wav is also supported
+   
+    snd.play()//plays the sound
+   
+    }
